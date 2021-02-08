60d2bd1a9a3700bfa1dc310dbbdec85e614ac33e
kavania2002 <72624080+kavania2002@users.noreply.github.com>
2021-02-08 08:34:48 +0530

    Added the remaining files

---
+++ MobiMart/static/js/main.js
@@ -0,0 +1,39 @@
+var updateBtns = document.getElementsByClassName("update-cart")
+
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId: ', productId, 'action: ', action)
+
+        if (user === 'AnonymousUser') {
+            console.log("Need to Login")
+        } else {
+            updateUserOrder(productId, action)
+            console.log("Successfull")
+        }
+    })
+}
+
+function updateUserOrder(productId, action) {
+    console.log('User is logged in, sending data...')
+
+    var url = 'updateItem'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+    .then((respone) => {
+        return respone.json()
+    })
+
+    .then((data) => {
+        console.log('data:', data)
+        location.reload()
+    })
+}
