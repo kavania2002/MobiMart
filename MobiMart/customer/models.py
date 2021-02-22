a07fbdb593b8db9d5b8366cbd4e6d8c93e8280e8
swetabarnwal <sweta_bt18@iiitkalyani.ac.in>
2021-02-22 22:57:54 +0530

    Updated samsung add to cart button

--- MobiMart/customer/models.py
+++ MobiMart/customer/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200,null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(max_length=200,null=False)
     email =models.EmailField(max_length=100,null=True)
 
     def __str__(self):
