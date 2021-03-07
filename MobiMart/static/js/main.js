e69b7f9b6393ae290552434be45425d1191f20ac
Qazi Maaz Arshad <56196640+QAZIMAAZARSHAD@users.noreply.github.com>
2021-03-07 20:04:09 +0530

    Added scroll-up function

--- MobiMart/static/js/main.js
+++ MobiMart/static/js/main.js
@@ -46,3 +46,11 @@ document.addEventListener('DOMContentLoaded', function () {
         main.classList.toggle('dark-theme')
     })
 }) 
+
+
+// click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
